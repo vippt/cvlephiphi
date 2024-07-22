43d504fb18d2d99006481c2e6a552cdd0271b0b5
vippt <lephiphi.com.2017@gmail.com>
2024-07-22 20:39:05 +0700

    Add files via upload

---
+++ pwa-app.js
@@ -0,0 +1,1 @@
+if('serviceWorker'in navigator){navigator.serviceWorker.register('https://raw.githubusercontent.com/vippt/cvlephiphi/main/pwa-sw.js').then(reg=>console.log('service worker registered')).catch(err=>console.log('service worker not registered',err));}
