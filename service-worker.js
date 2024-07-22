b78f355c92ee017d27f5cd95ca8bd23bb8098fab
vippt <lephiphi.com.2017@gmail.com>
2024-07-22 21:07:51 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
-self.addEventListener('install', (event) => {
-  console.log('Service worker installing...');
-  // Add a call to skipWaiting here if you have an immediate update strategy
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('your-cache-name').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/',
+        'https://lephiphi.com/'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activating...');
-  // Claim clients to start controlling them without reloading
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
-  // Implement caching strategies here
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
